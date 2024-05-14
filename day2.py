95a9760d4fbbcd7e4d075816612c44b06b2d08c3
karanfrom <karantiwari.it26@jecrc.ac.in>
2024-05-14 15:11:04 +0530

    sender file upload

---
+++ day2.py
@@ -0,0 +1,43 @@
+# var2=25.21
+# var1=10
+# var3=True
+# var4='UPFLAIRS'
+# var5='jecrc'
+# var6="""jecrc foundation"""
+# print(var1)
+# print(var2)
+# print(var3)
+# print(var4)
+# print(var5)
+# print(var6)
+# print()
+# print(type(var1))
+# print(type(var2))
+# print(type(var3))
+# print(type(var4))
+# print(type(var5))
+# print(type(var6))
+
+# rupees=25000
+# print("i have",rupees,"rupees")
+# paragraph="""hello we are from jecrc
+# welcome to my college
+# hello good be"""
+# print(paragraph)
+# college="jecrc foundation"
+# print(college.upper())
+# print(college.lower())
+# print(len(college))
+# print(college.count('c'))
+# print(college.endswith('a'))
+# print(college.startswith('j'))
+# college="jecrc foundation"
+# print(college.strip())
+# print(college.lstrip())
+# print(college.rstrip())
+college='JECRC'
+print(college[2])  #indexing
+print(college[0:5:2])  #step
+print(college[-3])
+print(college[-2:])
+print(college[::-1])
